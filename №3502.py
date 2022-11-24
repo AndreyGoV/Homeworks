b9fce5bbb78f75d3d46cda8223b3f81a6c7d7084
AndreyGoV <118801577+AndreyGoV@users.noreply.github.com>
2022-11-24 16:51:09 +0300

    Add files via upload

---
+++ №3502.py
@@ -0,0 +1,9 @@
+number_1 = int(input('Enter number 1:\n'))
+number_2 = int(input('Enter number 2:\n'))
+
+if number_1 > number_2:
+    print(1)
+elif number_1 < number_2:
+    print(2)
+else:
+    print(0)
