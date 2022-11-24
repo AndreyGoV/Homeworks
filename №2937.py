8946c7bfc74544a4a0a62825ab98b78a7905e837
AndreyGoV <118801577+AndreyGoV@users.noreply.github.com>
2022-11-24 16:21:49 +0300

    Add files via upload

---
+++ №2937.py
@@ -0,0 +1,2 @@
+number = int(input('Введите целое число от 0 до 10000:\n'))
+print(f'The next number for the number {number} is {number + 1}.\nThe previous number for the number {number} is {number - 1}.')
